e734911cfd0ff1b4fb00b5ba114a0537f41a7ef0
Christian Legnitto <christian@legnitto.com>
2025-02-04 10:25:00 -0800

    Fix formatting

--- tests/difftests/pipelines/compute/hardcoded/src/lib.rs
+++ tests/difftests/pipelines/compute/hardcoded/src/lib.rs
@@ -2,8 +2,6 @@
 use spirv_std::spirv;
 
 #[spirv(compute(threads(64)))]
-pub fn main_cs(
-    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] data: &mut [u32],
-) {
+pub fn main_cs(#[spirv(storage_buffer, descriptor_set = 0, binding = 0)] data: &mut [u32]) {
     data[0] = 42;
 }
