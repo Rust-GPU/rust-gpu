8f93dcae0082bb60d1e699e430df6e3a2a013e7f
Firestar99 <firestar99@sydow.cloud>
2025-04-10 16:16:19 +0200

    derivative: refactor derivatives, allows f32, Vec2, Vec3, Vec4 and Vec3A to be derived
    disallow f64 as spec expects only 32bit floats

--- crates/spirv-std/src/sealed.rs
+++ crates/spirv-std/src/sealed.rs
@@ -13,3 +13,18 @@ impl Sealed for i8 {}
 impl Sealed for i16 {}
 impl Sealed for i32 {}
 impl Sealed for i64 {}
+
+impl Sealed for glam::Vec2 {}
+impl Sealed for glam::Vec3 {}
+impl Sealed for glam::Vec4 {}
+impl Sealed for glam::DVec2 {}
+impl Sealed for glam::DVec3 {}
+impl Sealed for glam::DVec4 {}
+impl Sealed for glam::UVec2 {}
+impl Sealed for glam::UVec3 {}
+impl Sealed for glam::UVec4 {}
+impl Sealed for glam::IVec2 {}
+impl Sealed for glam::IVec3 {}
+impl Sealed for glam::IVec4 {}
+
+impl Sealed for glam::Vec3A {}
