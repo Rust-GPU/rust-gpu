99d91a13019d162086e8dce94dd0206edbe4d4be
Christian Legnitto <christian@legnitto.com>
2025-04-12 13:07:47 -0400

    Update tests

--- tests/ui/lang/issue-46.rs
+++ tests/ui/lang/issue-46.rs
@@ -2,7 +2,14 @@
 
 use spirv_std::spirv;
 
+#[derive(Default)]
+struct Foo {
+    bar: bool,
+    baz: [[u32; 2]; 1],
+}
+
 #[spirv(fragment)]
 pub fn main() {
     let x = [[1; 2]; 1];
+    let y = [Foo::default(); 1];
 }
