45c42cad41810d9ea585e25aa3af0f4e8e375e56
Eduard-Mihai Burtescu <eddyb@lyken.rs>
2025-05-12 19:50:09 +0300

    rustup: update to `nightly-2025-05-09` (~1.88).

--- crates/rustc_codegen_spirv/build.rs
+++ crates/rustc_codegen_spirv/build.rs
@@ -18,9 +18,9 @@ use std::{env, fs, mem};
 /// `cargo publish`. We need to figure out a way to do this properly, but let's hardcode it for now :/
 //const REQUIRED_RUST_TOOLCHAIN: &str = include_str!("../../rust-toolchain.toml");
 const REQUIRED_RUST_TOOLCHAIN: &str = r#"[toolchain]
-channel = "nightly-2025-04-28"
+channel = "nightly-2025-05-09"
 components = ["rust-src", "rustc-dev", "llvm-tools"]
-# commit_hash = cb31a009e3e735ab08613cec2d8a5a754e65596f"#;
+# commit_hash = 50aa04180709189a03dde5fd1c05751b2625ed37"#;
 
 fn rustc_output(arg: &str) -> Result<String, Box<dyn Error>> {
     let rustc = env::var("RUSTC").unwrap_or_else(|_| "rustc".into());
