84d80fa31ec1a89be45bc74a5538b21b052a7e71
Christian Legnitto <christian@legnitto.com>
2025-06-12 05:29:17 -0700

    Add other query cases to tests

---
+++ tests/compiletests/ui/image/query/rect_image_query_size.rs
@@ -0,0 +1,24 @@
+// build-pass
+// compile-flags: -C target-feature=+ImageQuery,+SampledRect
+// ignore-vulkan1.0
+// ignore-vulkan1.1
+// ignore-vulkan1.1spv1.4
+// ignore-vulkan1.2
+
+use spirv_std::spirv;
+use spirv_std::{Image, arch};
+
+#[spirv(fragment)]
+pub fn main(
+    #[spirv(descriptor_set = 0, binding = 0)] rect_storage: &Image!(rect, type=f32, sampled=false),
+    #[spirv(descriptor_set = 1, binding = 1)] rect_storage_array: &Image!(rect, type=f32, sampled=false, arrayed),
+    output: &mut glam::UVec3,
+) {
+    // Rect images only support query_size (not query_size_lod)
+    let rect_size: glam::UVec2 = rect_storage.query_size();
+
+    // Arrayed rect images return 3D size (width, height, array_layers)
+    let rect_array_size: glam::UVec3 = rect_storage_array.query_size();
+
+    *output = glam::UVec3::new(rect_size.x, rect_size.y, rect_array_size.z);
+}
---
+++ tests/compiletests/ui/image/query/sampled_image_rect_query_size_lod_err.rs
@@ -0,0 +1,20 @@
+// build-fail
+// normalize-stderr-test "\S*/crates/spirv-std/src/" -> "$SPIRV_STD_SRC/"
+// compile-flags: -C target-feature=+ImageQuery,+SampledRect
+// ignore-vulkan1.0
+// ignore-vulkan1.1
+// ignore-vulkan1.1spv1.4
+// ignore-vulkan1.2
+
+use spirv_std::{Image, arch, image::SampledImage, spirv};
+
+#[spirv(fragment)]
+pub fn main(
+    #[spirv(descriptor_set = 0, binding = 0)] rect_sampled: &SampledImage<
+        Image!(rect, type=f32, sampled),
+    >,
+    output: &mut glam::UVec2,
+) {
+    // This should fail because rect images don't support query_size_lod
+    *output = rect_sampled.query_size_lod(0);
+}
