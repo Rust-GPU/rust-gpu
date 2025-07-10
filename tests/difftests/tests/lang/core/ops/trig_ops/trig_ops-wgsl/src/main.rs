581cf4a668615374edd582310977572945a121a8
Christian Legnitto <christian@legnitto.com>
2025-07-10 09:27:59 +0200

    Rustfmt after rebasing on new rust version

--- tests/difftests/tests/lang/core/ops/trig_ops/trig_ops-wgsl/src/main.rs
+++ tests/difftests/tests/lang/core/ops/trig_ops/trig_ops-wgsl/src/main.rs
@@ -5,11 +5,11 @@ fn main() {
     let config = Config::from_path(std::env::args().nth(1).unwrap()).unwrap();
 
     let buffer_size = 1024;
-    let test =
-        WgpuComputeTestMultiBuffer::new_with_sizes(WgslComputeShader::default(), [64, 1, 1], &[
-            buffer_size,
-            buffer_size,
-        ]);
+    let test = WgpuComputeTestMultiBuffer::new_with_sizes(
+        WgslComputeShader::default(),
+        [64, 1, 1],
+        &[buffer_size, buffer_size],
+    );
 
     test.run_test(&config).unwrap();
 }
