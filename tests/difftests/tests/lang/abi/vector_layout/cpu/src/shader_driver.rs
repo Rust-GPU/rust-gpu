1e1922fb94d0a67703024eeccf696c5566611c0d
firestar99 <firestar99@sydow.cloud>
2025-09-08 16:57:42 +0200

    abi layout difftest: add member offset checking

--- tests/difftests/tests/lang/abi/vector_layout/cpu/src/shader_driver.rs
+++ tests/difftests/tests/lang/abi/vector_layout/cpu/src/shader_driver.rs
@@ -1,4 +1,4 @@
-use crate::layout::EvalResult;
+use crate::layout::{LAYOUT_COUNT, LAYOUT_LEN};
 use difftest::config::Config;
 use difftest::scaffold::compute::{BufferConfig, RustComputeShader, WgpuComputeTestMultiBuffer};
 
@@ -6,8 +6,8 @@ pub fn run() {
     let config = Config::from_path(std::env::args().nth(1).unwrap()).unwrap();
     let test = WgpuComputeTestMultiBuffer::new(
         RustComputeShader::default(),
-        [1, 1, 1],
-        Vec::from(&[BufferConfig::writeback(size_of::<EvalResult>())]),
+        [LAYOUT_COUNT as u32, 1, 1],
+        Vec::from(&[BufferConfig::writeback(LAYOUT_LEN * size_of::<u32>())]),
     );
     test.run_test(&config).unwrap();
 }
