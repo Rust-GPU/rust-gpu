fe99a69a4eb998aee281e160008e5458d7071c87
firestar99 <firestar99@sydow.cloud>
2025-09-08 16:57:42 +0200

    abi layout difftest: add member offset checking

--- tests/difftests/tests/lang/abi/vector_layout/cpu/src/shader.rs
+++ tests/difftests/tests/lang/abi/vector_layout/cpu/src/shader.rs
@@ -1,7 +1,11 @@
-use crate::layout::{EvalResult, eval_layouts};
+use crate::layout::eval_layouts;
+use spirv_std::glam::UVec3;
 use spirv_std::spirv;
 
 #[spirv(compute(threads(1)))]
-pub fn main_cs(#[spirv(storage_buffer, descriptor_set = 0, binding = 0)] output: &mut EvalResult) {
-    *output = eval_layouts();
+pub fn main_cs(
+    #[spirv(workgroup_id)] gid: UVec3,
+    #[spirv(storage_buffer, descriptor_set = 0, binding = 0)] output: &mut [u32],
+) {
+    eval_layouts(gid.x, output);
 }
