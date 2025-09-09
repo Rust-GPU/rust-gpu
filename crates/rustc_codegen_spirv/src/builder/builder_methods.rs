0546ebd00c21ed3291d8d36fd110aca80bdf3972
Eduard-Mihai Burtescu <eddyb@vectorware.com>
2025-09-09 11:40:02 +0300

    Replace `SpirvValueKind::IllegalTypeUsed` with mere `undef`.

--- crates/rustc_codegen_spirv/src/builder/builder_methods.rs
+++ crates/rustc_codegen_spirv/src/builder/builder_methods.rs
@@ -3411,10 +3411,7 @@ impl<'a, 'tcx> BuilderMethods<'a, 'tcx> for Builder<'a, 'tcx> {
         if buffer_store_intrinsic {
             self.codegen_buffer_store_intrinsic(fn_abi, args);
             let void_ty = SpirvType::Void.def(rustc_span::DUMMY_SP, self);
-            return SpirvValue {
-                kind: SpirvValueKind::IllegalTypeUsed(void_ty),
-                ty: void_ty,
-            };
+            return self.undef(void_ty);
         }
 
         if let Some((source_ty, target_ty)) = from_trait_impl {
