56563c9517db2c0d58002aeff2d448d4d1debfb1
firestar99 <firestar99@vectorware.com>
2025-09-17 18:05:41 +0200

    abi layout difftest: add all remaining glam types

--- tests/difftests/tests/lang/abi/vector_layout/cpu/src/layout.rs
+++ tests/difftests/tests/lang/abi/vector_layout/cpu/src/layout.rs
@@ -30,7 +30,7 @@ macro_rules! write_layout {
 }
 
 /// gid is checked between `0..LAYOUT_COUNT`
-pub const LAYOUT_COUNT: usize = 0x40;
+pub const LAYOUT_COUNT: usize = 0x60;
 /// at byte offset 0x100 * N starts layout N
 pub const LAYOUT_MAX_SIZE: usize = 0x100 / 0x4;
 pub const LAYOUT_LEN: usize = LAYOUT_COUNT * LAYOUT_MAX_SIZE;
@@ -52,10 +52,74 @@ pub fn eval_layouts(gid: u32, out: &mut [u32]) {
         0xC => write_layout!(out, offset, UVec4(x, y, z, w)),
         0xD => write_layout!(out, offset, IVec4(x, y, z, w)),
         0xE => write_layout!(out, offset, Vec4()), // private members
+        0xF => write_layout!(out, offset, Quat()), // private members
 
         // experiments structs
         0x10 => write_layout!(out, offset, Struct0x10(a, b)),
         0x11 => write_layout!(out, offset, Struct0x11(a, b)),
+        0x12 => write_layout!(out, offset, Struct0x12(a, b, c, d, e)),
+        0x13 => write_layout!(out, offset, Struct0x13(a)),
+
+        // mat
+        0x20 => write_layout!(out, offset, Mat2()), // private members
+        0x21 => write_layout!(out, offset, Mat3(x_axis, y_axis, z_axis)),
+        0x22 => write_layout!(out, offset, Mat3A(x_axis, y_axis, z_axis)),
+        0x23 => write_layout!(out, offset, Mat4(x_axis, y_axis, z_axis, w_axis)),
+        // f64
+        0x24 => write_layout!(out, offset, f64()),
+        0x25 => write_layout!(out, offset, DVec2(x, y)),
+        0x26 => write_layout!(out, offset, DVec3(x, y, z)),
+        0x27 => write_layout!(out, offset, DVec4(x, y, z, w)),
+        0x28 => write_layout!(out, offset, DQuat(x, y, z, w)),
+        0x29 => write_layout!(out, offset, DMat2()), // private members
+        0x2a => write_layout!(out, offset, DMat3(x_axis, y_axis, z_axis)),
+        0x2b => write_layout!(out, offset, DMat4(x_axis, y_axis, z_axis, w_axis)),
+
+        // i8
+        0x30 => write_layout!(out, offset, i8()),
+        0x31 => write_layout!(out, offset, I8Vec2(x, y)),
+        0x32 => write_layout!(out, offset, I8Vec3(x, y, z)),
+        0x33 => write_layout!(out, offset, I8Vec4(x, y, z, w)),
+        // i16
+        0x34 => write_layout!(out, offset, i16()),
+        0x35 => write_layout!(out, offset, I16Vec2(x, y)),
+        0x36 => write_layout!(out, offset, I16Vec3(x, y, z)),
+        0x37 => write_layout!(out, offset, I16Vec4(x, y, z, w)),
+        // i64
+        0x38 => write_layout!(out, offset, i64()),
+        0x39 => write_layout!(out, offset, I64Vec2(x, y)),
+        0x3a => write_layout!(out, offset, I64Vec3(x, y, z)),
+        0x3b => write_layout!(out, offset, I64Vec4(x, y, z, w)),
+
+        // u8
+        0x40 => write_layout!(out, offset, u8()),
+        0x41 => write_layout!(out, offset, U8Vec2(x, y)),
+        0x42 => write_layout!(out, offset, U8Vec3(x, y, z)),
+        0x43 => write_layout!(out, offset, U8Vec4(x, y, z, w)),
+        // u16
+        0x44 => write_layout!(out, offset, u16()),
+        0x45 => write_layout!(out, offset, U16Vec2(x, y)),
+        0x46 => write_layout!(out, offset, U16Vec3(x, y, z)),
+        0x47 => write_layout!(out, offset, U16Vec4(x, y, z, w)),
+        // u64
+        0x48 => write_layout!(out, offset, u64()),
+        0x49 => write_layout!(out, offset, U64Vec2(x, y)),
+        0x4a => write_layout!(out, offset, U64Vec3(x, y, z)),
+        0x4b => write_layout!(out, offset, U64Vec4(x, y, z, w)),
+        // Affine
+        0x4c => write_layout!(out, offset, Affine2(matrix2, translation)),
+        0x4d => write_layout!(out, offset, Affine3A(matrix3, translation)),
+        0x4e => write_layout!(out, offset, DAffine2(matrix2, translation)),
+        0x4f => write_layout!(out, offset, DAffine3(matrix3, translation)),
+
+        // bool
+        0x50 => write_layout!(out, offset, bool()),
+        0x51 => write_layout!(out, offset, BVec2(x, y)),
+        0x52 => write_layout!(out, offset, BVec3(x, y, z)),
+        0x53 => write_layout!(out, offset, BVec3A()), // private members
+        0x54 => write_layout!(out, offset, BVec4(x, y, z, w)),
+        0x55 => write_layout!(out, offset, BVec4A()),
+
         _ => {}
     }
 }
@@ -79,5 +143,20 @@ mod experiments {
         /// * 4 (CPU): 16 size, 4 alignment, 12 b offset
         /// * 16 (GPU): 32 size, 16 alignment, 16 b offset
         pub b: f32,
+    }
+
+    #[repr(C)]
+    pub struct Struct0x12 {
+        pub a: BVec3,
+        pub b: f32,
+        pub c: BVec2,
+        pub d: f32,
+        pub e: BVec4,
+    }
+
+    #[repr(C)]
+    #[repr(align(16))]
+    pub struct Struct0x13 {
+        pub a: f32,
     }
 }
