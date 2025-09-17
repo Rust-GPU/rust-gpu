c526055bed613dfada2785b5d45036e9dd230f50
firestar99 <firestar99@vectorware.com>
2025-09-17 12:46:42 +0200

    asm vector: adjust compiletest `dis/complex_image_sample_inst`, remove non-sense ConstOffset

--- tests/compiletests/ui/dis/complex_image_sample_inst.rs
+++ tests/compiletests/ui/dis/complex_image_sample_inst.rs
@@ -5,13 +5,7 @@
 use core::arch::asm;
 use spirv_std::spirv;
 
-fn sample_proj_lod(
-    coord: glam::Vec4,
-    ddx: glam::Vec2,
-    ddy: glam::Vec2,
-    offset_x: i32,
-    offset_y: i32,
-) -> glam::Vec4 {
+fn sample_proj_lod(coord: glam::Vec4, ddx: glam::Vec2, ddy: glam::Vec2) -> glam::Vec4 {
     unsafe {
         let mut result = glam::Vec4::default();
         let index = 0u32;
@@ -19,11 +13,8 @@ fn sample_proj_lod(
             "OpDecorate %image_2d_var DescriptorSet 0",
             "OpDecorate %image_2d_var Binding 0",
             "%uint                  = OpTypeInt 32 0",
-            "%int                   = OpTypeInt 32 1",
             "%float                 = OpTypeFloat 32",
-            "%v2int                 = OpTypeVector %int 2",
             "%uint_0                = OpConstant %uint 0",
-            "%int_0                 = OpConstant %int 0",
             "%image_2d              = OpTypeImage %float Dim2D 0 0 0 1 Unknown",
             "%sampled_image_2d      = OpTypeSampledImage %image_2d",
             "%image_array           = OpTypeRuntimeArray %sampled_image_2d",
@@ -40,23 +31,18 @@ fn sample_proj_lod(
             "%coord                 = OpLoad _ {0}",
             "%ddx                   = OpLoad _ {3}",
             "%ddy                   = OpLoad _ {4}",
-            "%offset_x              = OpLoad _ {5}",
-            "%offset_y              = OpLoad _ {6}",
-            "%const_offset          = OpConstantComposite %v2int %int_0 %int_0",
-            "%result                = OpImageSampleProjExplicitLod _ %25 %coord Grad|ConstOffset %ddx %ddy %const_offset",
+            "%result                = OpImageSampleProjExplicitLod typeof*{2} %25 %coord Grad %ddx %ddy",
             "OpStore {2} %result",
             in(reg) &coord,
             in(reg) &index,
             in(reg) &mut result,
             in(reg) &ddx,
             in(reg) &ddy,
-            in(reg) &offset_x,
-            in(reg) &offset_y,
         );
         result
     }
 }
 #[spirv(fragment)]
 pub fn main() {
-    sample_proj_lod(glam::Vec4::ZERO, glam::Vec2::ZERO, glam::Vec2::ZERO, 0, 0);
+    sample_proj_lod(glam::Vec4::ZERO, glam::Vec2::ZERO, glam::Vec2::ZERO);
 }
