9218e64b5946bb973d864da551f62d9f496cb80f
firestar99 <firestar99@vectorware.com>
2025-09-24 10:21:33 +0200

    abi layout: minor code cleanups

--- tests/difftests/tests/lang/abi/vector_layout/cpu/src/cpu_driver.rs
+++ tests/difftests/tests/lang/abi/vector_layout/cpu/src/cpu_driver.rs
@@ -1,12 +1,12 @@
 use crate::glam_features::GlamFeatures;
-use crate::layout::{LAYOUT_COUNT, LAYOUT_LEN, eval_layouts};
+use crate::layout::{LAYOUT_LEN, LAYOUT_RANGE, eval_layouts};
 use difftest::config::Config;
 
 pub fn run(glam_feature: GlamFeatures) {
     glam_feature.assert();
     let config = Config::from_path(std::env::args().nth(1).unwrap()).unwrap();
     let mut out = vec![0; LAYOUT_LEN];
-    for gid in 0..LAYOUT_COUNT {
+    for gid in LAYOUT_RANGE {
         eval_layouts(gid as u32, &mut out);
     }
     config.write_result(&out).unwrap()
