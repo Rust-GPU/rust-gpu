4679e0f6845c0aded3108473314af1c741335ea7
firestar99 <firestar99@vectorware.com>
2025-09-24 10:21:33 +0200

    abi layout: minor code cleanups

--- tests/difftests/tests/lang/abi/vector_layout/cpu/src/shader_driver.rs
+++ tests/difftests/tests/lang/abi/vector_layout/cpu/src/shader_driver.rs
@@ -1,14 +1,15 @@
 use crate::glam_features::GlamFeatures;
-use crate::layout::{LAYOUT_COUNT, LAYOUT_LEN};
+use crate::layout::{LAYOUT_LEN, LAYOUT_RANGE};
 use difftest::config::Config;
 use difftest::scaffold::compute::{BufferConfig, RustComputeShader, WgpuComputeTestMultiBuffer};
 
 pub fn run(glam_feature: GlamFeatures) {
     glam_feature.assert();
     let config = Config::from_path(std::env::args().nth(1).unwrap()).unwrap();
+    assert_eq!(0, LAYOUT_RANGE.start);
     let test = WgpuComputeTestMultiBuffer::new(
         RustComputeShader::default(),
-        [LAYOUT_COUNT as u32, 1, 1],
+        [LAYOUT_RANGE.end as u32, 1, 1],
         Vec::from(&[BufferConfig::writeback(LAYOUT_LEN * size_of::<u32>())]),
     );
     test.run_test(&config).unwrap();
