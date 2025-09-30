c00816c7056d68d672454f9ce0147f526dd79ca3
firestar99 <firestar99@vectorware.com>
2025-09-30 10:58:28 +0200

    ash runner: make debug msgs easier to read

--- examples/runners/ash/src/main.rs
+++ examples/runners/ash/src/main.rs
@@ -1309,33 +1309,21 @@ unsafe fn any_as_u8_slice<T: Sized>(p: &T) -> &[u8] {
 
 unsafe extern "system" fn vulkan_debug_callback(
     message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
-    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    _message_type: vk::DebugUtilsMessageTypeFlagsEXT,
     p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT<'_>,
     _user_data: *mut std::os::raw::c_void,
 ) -> vk::Bool32 {
     let callback_data = unsafe { *p_callback_data };
-    let message_id_number: i32 = callback_data.message_id_number;
-
     let message_id_name = if callback_data.p_message_id_name.is_null() {
         Cow::from("")
     } else {
         unsafe { CStr::from_ptr(callback_data.p_message_id_name).to_string_lossy() }
     };
-
     let message = if callback_data.p_message.is_null() {
         Cow::from("")
     } else {
         unsafe { CStr::from_ptr(callback_data.p_message).to_string_lossy() }
     };
-
-    println!(
-        "{:?}:\n{:?} [{} ({})] : {}\n",
-        message_severity,
-        message_type,
-        message_id_name,
-        &message_id_number.to_string(),
-        message,
-    );
-
+    println!("{message_severity:?}: [{message_id_name}] : {message}");
     vk::FALSE
 }
