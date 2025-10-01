c1d2b480613aaeb7baf27cb887dcfba9260b95d0
firestar99 <firestar99@vectorware.com>
2025-10-01 14:56:23 +0200

    ash & wgpu runner: remove build script forwarding `PROFILE` env var
    no longer necessary since d416b65572279c8bd5d1b7df9c04800d62061db5

--- examples/runners/wgpu/build.rs
+++ examples/runners/wgpu/build.rs
@@ -8,13 +8,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-env-changed=CARGO_CFG_TARGET_OS");
     println!("cargo:rerun-if-env-changed=CARGO_CFG_TARGET_ARCH");
-    // While OUT_DIR is set for both build.rs and compiling the crate, PROFILE is only set in
-    // build.rs. So, export it to crate compilation as well.
-    let profile = env::var("PROFILE").unwrap();
-    println!("cargo:rustc-env=PROFILE={profile}");
+
     if target_os != "android" && target_arch != "wasm32" {
         return Ok(());
     }
+    let profile = env::var("PROFILE").unwrap();
     let mut dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     // Strip `$profile/build/*/out`.
     let ok = dir.ends_with("out")
