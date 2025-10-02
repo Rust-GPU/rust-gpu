fb61b79127469e691e326eed696ede4e01d52d70
firestar99 <firestar99@vectorware.com>
2025-10-02 12:44:08 +0200

    ash runner: cleanup debug callback

--- examples/runners/ash/src/device.rs
+++ examples/runners/ash/src/device.rs
@@ -169,17 +169,16 @@ unsafe extern "system" fn vulkan_debug_callback(
     p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT<'_>,
     _user_data: *mut std::os::raw::c_void,
 ) -> vk::Bool32 {
-    let callback_data = unsafe { *p_callback_data };
-    let message_id_name = if callback_data.p_message_id_name.is_null() {
-        Cow::from("")
-    } else {
-        unsafe { CStr::from_ptr(callback_data.p_message_id_name).to_string_lossy() }
-    };
-    let message = if callback_data.p_message.is_null() {
-        Cow::from("")
-    } else {
-        unsafe { CStr::from_ptr(callback_data.p_message).to_string_lossy() }
-    };
-    println!("{message_severity:?}: [{message_id_name}] : {message}");
-    vk::FALSE
+    unsafe {
+        let callback_data = *p_callback_data;
+        let message_id_name = callback_data
+            .message_id_name_as_c_str()
+            .map_or(Cow::Borrowed(""), CStr::to_string_lossy);
+        let message = callback_data
+            .message_as_c_str()
+            .map_or(Cow::Borrowed(""), CStr::to_string_lossy);
+
+        println!("{message_severity:?}: [{message_id_name}] : {message}");
+        vk::FALSE
+    }
 }
