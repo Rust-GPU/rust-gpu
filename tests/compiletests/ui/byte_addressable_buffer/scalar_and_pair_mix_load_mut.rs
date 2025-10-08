7a72a2fa401c51f008fd92960ddbba646d9f3a79
firestar99 <firestar99@vectorware.com>
2025-10-08 11:17:41 +0200

    byte_addr_buffer: compiletests with mixed scalar and scalar pairs, store is failing

---
+++ tests/compiletests/ui/byte_addressable_buffer/scalar_and_pair_mix_load_mut.rs
@@ -0,0 +1,32 @@
+// build-pass
+
+use spirv_std::ByteAddressableBuffer;
+use spirv_std::spirv;
+
+#[derive(Copy, Clone, Debug)]
+pub struct MyScalar(i32);
+
+#[spirv(fragment)]
+pub fn load_scalar(
+    #[spirv(descriptor_set = 0, binding = 0, storage_buffer)] buf: &mut [u32],
+    scalar: &mut MyScalar,
+) {
+    unsafe {
+        let mut buf = ByteAddressableBuffer::from_mut_slice(buf);
+        *scalar = buf.load(5);
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct MyScalarPair(i32, i32);
+
+#[spirv(fragment)]
+pub fn load_scalar_pair(
+    #[spirv(descriptor_set = 0, binding = 0, storage_buffer)] buf: &mut [u32],
+    scalar_pair: &mut MyScalarPair,
+) {
+    unsafe {
+        let mut buf = ByteAddressableBuffer::from_mut_slice(buf);
+        *scalar_pair = buf.load(5);
+    }
+}
