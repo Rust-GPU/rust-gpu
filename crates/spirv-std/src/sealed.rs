08a245cb2303d5f2c1f744f5db1279b50aeef94b
firestar99 <firestar99@vectorware.com>
2025-10-14 15:01:01 +0200

    scalar or vector: move all scalar traits and impl macros to `mod scalar`

--- crates/spirv-std/src/sealed.rs
+++ crates/spirv-std/src/sealed.rs
@@ -2,18 +2,6 @@
 /// of `spirv-std`.
 pub trait Sealed {}
 
-impl Sealed for bool {}
-impl Sealed for f32 {}
-impl Sealed for f64 {}
-impl Sealed for u8 {}
-impl Sealed for u16 {}
-impl Sealed for u32 {}
-impl Sealed for u64 {}
-impl Sealed for i8 {}
-impl Sealed for i16 {}
-impl Sealed for i32 {}
-impl Sealed for i64 {}
-
 impl Sealed for glam::Vec2 {}
 impl Sealed for glam::Vec3 {}
 impl Sealed for glam::Vec4 {}
