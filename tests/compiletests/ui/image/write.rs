7efddcdea73d44512d612fe062338df12edb5c8c
firestar99 <firestar99@vectorware.com>
2025-12-01 12:33:28 +0100

    image texel write: change texel write arg from any vector to `SampleResult`, allows scalars
    * breakage: no longer allows any vector, type must match image format
    * allows texel writes into single-component scalar textures

--- tests/compiletests/ui/image/write.rs
+++ tests/compiletests/ui/image/write.rs
@@ -2,15 +2,16 @@
 // build-pass
 // compile-flags: -C target-feature=+StorageImageWriteWithoutFormat
 
+use spirv_std::glam::*;
 use spirv_std::spirv;
 use spirv_std::{Image, arch};
 
 #[spirv(fragment)]
 pub fn main(
-    texels: glam::Vec2,
+    texels: Vec2,
     #[spirv(descriptor_set = 0, binding = 0)] image: &Image!(2D, type=f32, sampled=false),
 ) {
     unsafe {
-        image.write(glam::UVec2::new(0, 1), texels);
+        image.write(UVec2::new(0, 1), Vec4::from((texels, 0., 0.)));
     }
 }
