04095dd7414b6c9ca8245fecaeb412ae55588c04
firestar99 <firestar99@vectorware.com>
2025-12-15 12:18:49 +0100

    target-spec: don't validate target env in spirv-builder

--- crates/rustc_codegen_spirv-types/src/target.rs
+++ crates/rustc_codegen_spirv-types/src/target.rs
@@ -3,12 +3,12 @@ use thiserror::Error;
 
 pub const SPIRV_TARGET_PREFIX: &str = "spirv-unknown-";
 
-/// A well-known rust-gpu target.
+/// A well-formed rust-gpu target.
 ///
-/// Even if [`Self::from_target`] or [`Self::from_env`] accepts the target, it can still fail to compile if the
-/// specific rust-gpu compiler in use offers no support for it.
-///
-/// While the current implementation is just a validated string, that may change in the future.
+/// The constructors [`Self::from_target`] or [`Self::from_env`] only check whether the target is well-formed, not
+/// whether it is valid. Since `spirv-builder` is backwards compatible with older rust-gpu compilers, only the compiler
+/// itself knows what targets it can and cannot support. This also allows adding new targets to the compiler without
+/// having to update `spirv-builder` and `cargo-gpu`.
 #[derive(Clone)]
 pub struct SpirvTarget {
     target: String,
@@ -16,36 +16,20 @@ pub struct SpirvTarget {
 
 impl SpirvTarget {
     pub fn from_target(target: &str) -> Result<Self, TargetError> {
-        let target_env = target.strip_prefix(SPIRV_TARGET_PREFIX).ok_or_else(|| {
+        let _target_env = target.strip_prefix(SPIRV_TARGET_PREFIX).ok_or_else(|| {
             TargetError::NonSpirvTarget {
                 target: target.to_string(),
             }
         })?;
-
-        // used only to split the full list into groups.
-        #[allow(clippy::match_same_arms)]
-        match target_env {
-            // HACK(firestar99) this hardcoded list should be replaced with patterns (eg. `vulkan{}.{}`),
-            // which would allow us to support new target versions without updating this list.
-            "spv1.0" | "spv1.1" | "spv1.2" | "spv1.3" | "spv1.4" | "spv1.5" | "spv1.6" => {}
-            "opengl4.0" | "opengl4.1" | "opengl4.2" | "opengl4.3" | "opengl4.5" => {}
-            "vulkan1.0" | "vulkan1.1" | "vulkan1.1spv1.4" | "vulkan1.2" | "vulkan1.3"
-            | "vulkan1.4" => {}
-
-            _ => {
-                return Err(TargetError::UnsupportedSpirvTargetEnv {
-                    target_env: target_env.into(),
-                });
-            }
-        }
-
         Ok(Self {
             target: target.to_string(),
         })
     }
 
     pub fn from_env(target_env: &str) -> Result<Self, TargetError> {
-        Self::from_target(&format!("{SPIRV_TARGET_PREFIX}{target_env}"))
+        Ok(Self {
+            target: format!("{SPIRV_TARGET_PREFIX}{target_env}"),
+        })
     }
 
     pub fn target(&self) -> &str {
@@ -68,6 +52,4 @@ impl Debug for SpirvTarget {
 pub enum TargetError {
     #[error("SPIR-V target must start with `{SPIRV_TARGET_PREFIX}...`, was `{target}`")]
     NonSpirvTarget { target: String },
-    #[error("SPIR-V target `{SPIRV_TARGET_PREFIX}-{target_env}` is not supported")]
-    UnsupportedSpirvTargetEnv { target_env: String },
 }
