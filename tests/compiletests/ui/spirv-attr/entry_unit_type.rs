195335a6f2279c4776f5bd1101d5189ac7081023
firestar99 <firestar99@vectorware.com>
2026-01-15 09:50:45 +0100

    allow zst entry params in input and output positions

---
+++ tests/compiletests/ui/spirv-attr/entry_unit_type.rs
@@ -0,0 +1,24 @@
+// build-pass
+// compile-flags: -C llvm-args=--disassemble
+// normalize-stderr-test "OpSource .*\n" -> ""
+// normalize-stderr-test "OpLine .*\n" -> ""
+// normalize-stderr-test "%\d+ = OpString .*\n" -> ""
+// normalize-stderr-test "(^|\n); .*" -> ""
+// normalize-stderr-test "OpCapability VulkanMemoryModel\n" -> ""
+// normalize-stderr-test "OpMemoryModel Logical Vulkan" -> "OpMemoryModel Logical Simple"
+// ignore-spv1.0
+// ignore-spv1.1
+// ignore-spv1.2
+// ignore-spv1.3
+// ignore-vulkan1.0
+// ignore-vulkan1.1
+
+use spirv_std::glam::*;
+use spirv_std::{Image, spirv};
+
+#[spirv(vertex)]
+pub fn main(in1: (), in2: u32, out1: &mut [f32; 3], out2: &mut (), out3: &mut f32) {
+    *out1 = Default::default();
+    *out2 = Default::default();
+    *out3 = in2 as f32;
+}
