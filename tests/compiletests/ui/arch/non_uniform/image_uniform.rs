31660854f67d7489ecf5d2a86d75c8d02d75030b
firestar99 <firestar99@sydow.cloud>
2026-02-18 19:06:38 +0100

    rm cap checks: add compiletests for non_uniform post-link removal

---
+++ tests/compiletests/ui/arch/non_uniform/image_uniform.rs
@@ -0,0 +1,31 @@
+// build-pass
+// compile-flags: -C llvm-args=--disassemble
+// compile-flags: -C target-feature=+RuntimeDescriptorArray,+SampledImageArrayDynamicIndexing
+// normalize-stderr-test "OpSource .*\n" -> ""
+// normalize-stderr-test "OpLine .*\n" -> ""
+// normalize-stderr-test "%\d+ = OpString .*\n" -> ""
+// normalize-stderr-test "; .*\n" -> ""
+// normalize-stderr-test "OpCapability VulkanMemoryModel\n" -> ""
+// normalize-stderr-test "OpMemoryModel Logical Vulkan" -> "OpMemoryModel Logical Simple"
+// ignore-spv1.0
+// ignore-spv1.1
+// ignore-spv1.2
+// ignore-spv1.3
+// ignore-spv1.4
+// ignore-vulkan1.0
+// ignore-vulkan1.1
+
+use spirv_std::glam::*;
+use spirv_std::image::*;
+use spirv_std::*;
+
+#[spirv(vertex)]
+pub fn main(
+    index: u32,
+    #[spirv(descriptor_set = 0, binding = 0)] images: &RuntimeArray<Image2d>,
+    out: &mut Vec4,
+) {
+    unsafe {
+        *out = images.index(index as usize).fetch(UVec2::new(1, 2));
+    }
+}
