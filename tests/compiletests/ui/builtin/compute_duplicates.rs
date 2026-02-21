ce89c4b3f0c349cdd005bf34a0ae917ff3580852
Alex Helfet <alex.helfet@gmail.com>
2026-02-21 17:51:43 +0000

    linker pass "remove_duplicate_builtin_variables" to fix compute_duplicates compiletest.

--- tests/compiletests/ui/builtin/compute_duplicates.rs
+++ tests/compiletests/ui/builtin/compute_duplicates.rs
@@ -1,7 +1,8 @@
 // build-pass
 // compile-flags: -C llvm-args=--disassemble
-// normalize-stderr-test "OpLine .*\n" -> ""
+
 // normalize-stderr-test "OpSource .*\n" -> ""
+// normalize-stderr-test "OpLine .*\n" -> ""
 // normalize-stderr-test "%\d+ = OpString .*\n" -> ""
 // normalize-stderr-test "OpCapability VulkanMemoryModel\n" -> ""
 // normalize-stderr-test "OpMemoryModel Logical Vulkan" -> "OpMemoryModel Logical Simple"
@@ -14,16 +15,30 @@ use spirv_std::{
 };
 
 #[spirv(compute(threads(1)))]
-pub fn compute(
-    // TODO: Compile error: duplicate builtin
-    #[spirv(local_invocation_index)] local_idx: u32,
+pub fn entry_1(
+    #[spirv(local_invocation_index)] _local_idx: u32,
 ) {
-    let _local_invocation_index: u32 = compute::local_invocation_index();
-    let _local_invocation_index: u32 = compute::local_invocation_index();
-    let _local_invocation_index: u32 = f();
+    let _: u32 = compute::local_invocation_index();
+    let _: u32 = compute::local_invocation_index();
+    let _: u32 = sub_1();
+    let _: u32 = sub_2();
+}
+
+#[spirv(compute(threads(1)))]
+pub fn entry_2(
+    #[spirv(local_invocation_index)] _local_idx: u32,
+) {
+    let _: u32 = compute::local_invocation_index();
+    let _: u32 = sub_1();
+    let _: u32 = sub_2();
 }
 
 #[inline(never)]
-fn f() -> u32 {
+fn sub_1() -> u32 {
+    compute::local_invocation_index()
+}
+
+#[inline(never)]
+fn sub_2() -> u32 {
     compute::local_invocation_index()
 }
