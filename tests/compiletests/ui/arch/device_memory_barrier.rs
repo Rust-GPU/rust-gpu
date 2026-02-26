bd97f845cdf8ca83fb418da44fb363881be9d29a
firestar99 <firestar99@sydow.cloud>
2026-02-26 10:38:20 +0100

    move mod: move `arch::barrier` to `barrier`

--- tests/compiletests/ui/arch/device_memory_barrier.rs
+++ tests/compiletests/ui/arch/device_memory_barrier.rs
@@ -1,11 +1,12 @@
 // build-pass
 // compile-flags: -C target-feature=+VulkanMemoryModelDeviceScopeKHR,+ext:SPV_KHR_vulkan_memory_model
 // compile-flags: -C llvm-args=--disassemble-fn=device_memory_barrier::device_memory_barrier
+// normalize-stderr-test "OpLine .*\n" -> ""
 
 use spirv_std::spirv;
 
 unsafe fn device_memory_barrier() {
-    spirv_std::arch::device_memory_barrier();
+    spirv_std::barrier::device_memory_barrier();
 }
 
 #[spirv(compute(threads(1, 1, 1)))]
