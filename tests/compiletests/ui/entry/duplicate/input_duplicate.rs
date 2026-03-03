c6a3112498e888b2b29f5167abe449e1ebf9d43c
firestar99 <firestar99@vectorware.com>
2026-03-03 14:07:16 +0100

    dedup OpVariable: add compiletests showing failure when both attr and getter are used

---
+++ tests/compiletests/ui/entry/duplicate/input_duplicate.rs
@@ -0,0 +1,23 @@
+// build-pass
+// compile-flags: -C llvm-args=--disassemble
+// normalize-stderr-test "OpLine .*\n" -> ""
+// normalize-stderr-test "OpSource .*\n" -> ""
+// normalize-stderr-test "%\d+ = OpString .*\n" -> ""
+// normalize-stderr-test "OpCapability VulkanMemoryModel\n" -> ""
+// normalize-stderr-test "OpExtension .SPV_KHR_vulkan_memory_model.\n" -> ""
+// normalize-stderr-test "OpMemoryModel Logical Vulkan" -> "OpMemoryModel Logical Simple"
+// normalize-stderr-test "; .*\n" -> ""
+
+// ignore-spv1.0
+// ignore-spv1.1
+// ignore-spv1.2
+// ignore-vulkan1.0
+// ignore-vulkan1.1
+
+use core::arch::asm;
+use spirv_std::{glam::*, spirv};
+
+#[spirv(fragment)]
+pub fn main(input1: f32, input2: f32, output: &mut f32) {
+    *output = input1 + input2;
+}
