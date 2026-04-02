1e203ebea92835f3e5c3245be0b12cfa57965e4f
LegNeato <368904+LegNeato@users.noreply.github.com>
2026-04-02 04:41:46 +0000

    Upgrade Rust toolchain to nightly-2026-04-02
    Updates:
    - rust-toolchain.toml: nightly-2026-04-02
    - commit hash: 7e46c5f6fb87f8cf4353e058479cef15d1d952b4
    - REQUIRED_RUST_TOOLCHAIN in build.rs

--- crates/rustc_codegen_spirv/build.rs
+++ crates/rustc_codegen_spirv/build.rs
@@ -19,9 +19,9 @@ use std::{env, fs, mem};
 /// `cargo publish`. We need to figure out a way to do this properly, but let's hardcode it for now :/
 //const REQUIRED_RUST_TOOLCHAIN: &str = include_str!("../../rust-toolchain.toml");
 const REQUIRED_RUST_TOOLCHAIN: &str = r#"[toolchain]
-channel = "nightly-2026-04-01"
+channel = "nightly-2026-04-02"
 components = ["rust-src", "rustc-dev", "llvm-tools"]
-# commit_hash = 48cc71ee88cd0f11217eced958b9930970da998b"#;
+# commit_hash = 7e46c5f6fb87f8cf4353e058479cef15d1d952b4"#;
 
 fn rustc_output(arg: &str) -> Result<String, Box<dyn Error>> {
     let rustc = env::var("RUSTC").unwrap_or_else(|_| "rustc".into());
