109c302fb9fcdc5666d338cece17e46114faf6f9
firestar99 <firestar99@vectorware.com>
2026-06-26 10:51:21 +0200

    patch out codegen_ssa debuginfo generation causing compile failures

--- crates/rustc_codegen_spirv/build.rs
+++ crates/rustc_codegen_spirv/build.rs
@@ -256,6 +256,10 @@ pub(super) fn elf_e_flags(architecture: Architecture, sess: &Session) -> u32 {",
                 );
             }
 
+            if relative_path == Path::new("src/mir/mod.rs") {
+                src = src.replace("fx.fill_function_debug_context();", "");
+            }
+
             fs::write(out_path, src)?;
         }
     }
